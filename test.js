412859db661f27471c788a1a47333713353daa00
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2017-08-28 00:50:51 -0700

    Now nyc will use the non-transpiled version

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 /* eslint-disable flowtype/require-parameter-type, flowtype/require-return-type, no-magic-numbers */
 import {test} from "tap"
 
-import replaceWhen from "./"
+import replaceWhen from "./index"
 
 const isEven = (value) => value % 2 === 0
 
