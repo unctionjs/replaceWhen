5a82d2e10a1f732c6b551e038982e018b951ef85
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2019-04-13 21:01:45 -0700

    Autolinting

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ import always from "@unction/always";
 import itself from "@unction/itself";
 import mapValues from "@unction/mapvalues";
 import ifThenElse from "@unction/ifthenelse";
-export default function replaceWhen(predicate) {
-  return function replaceWhenPredicate(replacement) {
+export default function replaceWhen (predicate) {
+  return function replaceWhenPredicate (replacement) {
     return mapValues(ifThenElse(predicate)(always(replacement))(itself));
   };
 }
--- test.js
+++ test.js
@@ -1,15 +1,15 @@
 /* eslint-disable no-magic-numbers */
-import {test} from "tap"
+import {test} from "tap";
 
-import replaceWhen from "./index"
+import replaceWhen from "./index";
 
-const isEven = (value) => value % 2 === 0
+const isEven = (value) => value % 2 === 0;
 
 test(({same, end}) => {
   same(
     replaceWhen(isEven)(null)([1, 2, 3]),
     [1, null, 3]
-  )
+  );
 
-  end()
-})
+  end();
+});
