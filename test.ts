0103d703cadce3a729283252b8900b9e3aa5099a
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2019-09-05 22:58:52 -0700

    Moving to jest and typing the test file

---
+++ test.ts
@@ -0,0 +1,8 @@
+/* eslint-disable no-magic-numbers */
+import replaceWhen from "./index.ts";
+
+const isEven = (value) => value % 2 === 0;
+
+test(() => {
+  expect(replaceWhen(isEven)(null)([1, 2, 3])).toEqual([1, null, 3]);
+});
